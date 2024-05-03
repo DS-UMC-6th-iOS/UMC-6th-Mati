d7393fed7de782baa24b8cce61d946ffcf220c65
miju <alwn8918@gmail.com>
2024-05-03 13:42:17 +0900

    week3_study

--- Protocol/ViewController.swift
+++ Protocol/ViewController.swift
@@ -7,11 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// 클래스 바깥에서 선언
+protocol Flyable {
+    func fly()
+}
 
+protocol Runable {
+    func run()
+}
+
+// Flyable 기능 추가
+class Person: Flyable, Runable {
+    func run() {
+        print("달렸.")
+    }
+    
+    func fly() {
+        print("날았다.")
+    }
+}
+
+protocol ChangeLabelDelegate {
+    func doChange()
+}
+
+class ViewController: UIViewController {
+    
+    let person = Person()
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func buttonGoNextVC(_ sender: Any) {
+        guard let nextVC = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController else {
+            return
+        }
+        nextVC.previousViewController = self
+        nextVC.modalPresentationStyle = .fullScreen
+        present(nextVC, animated: true, completion: nil)
+    }
+    
+    var delegate: ChangeLabelDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        person.fly()
+        person.run()
     }
 
 
