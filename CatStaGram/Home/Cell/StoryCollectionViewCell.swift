cf87f34f05c76bfd3997b15910f4e240af32c88f
miju <alwn8918@gmail.com>
2024-05-10 07:17:49 +0900

    week5_study

---
+++ CatStaGram/Home/Cell/StoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  StoryCollectionViewCell.swift
+//  CatStaGram
+//
+//  Created by 김미주 on 10/05/2024.
+//
+
+import UIKit
+
+class StoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
