0ad43e20730399904e6aeb269716f97cc8eebb3b
miju <alwn8918@gmail.com>
2024-05-10 08:25:50 +0900

    week3

---
+++ daangn/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  daangn
+//
+//  Created by 김미주 on 10/05/2024.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
