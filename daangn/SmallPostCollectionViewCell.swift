aa1d48006b47b3f4873b276a2bbbc966faecb434
miju <alwn8918@gmail.com>
2024-05-10 20:36:31 +0900

    week4

---
+++ daangn/SmallPostCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SmallPostCollectionViewCell.swift
+//  daangn
+//
+//  Created by 김미주 on 10/05/2024.
+//
+
+import UIKit
+
+class SmallPostCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
