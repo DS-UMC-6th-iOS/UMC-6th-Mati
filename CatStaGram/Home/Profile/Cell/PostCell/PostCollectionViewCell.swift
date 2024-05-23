4470dcf8cdb8a5ecf9f020074b5f44bf2efdaa92
miju <alwn8918@gmail.com>
2024-05-24 05:43:35 +0900

    week7

---
+++ CatStaGram/Home/Profile/Cell/PostCell/PostCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PostCollectionViewCell.swift
+//  CatStaGram
+//
+//  Created by 김미주 on 24/05/2024.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
