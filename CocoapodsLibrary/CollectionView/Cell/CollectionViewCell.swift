467ca3ff9b4c6738d9ca47b06445374f4e49f8f4
miju <alwn8918@gmail.com>
2024-05-25 06:19:22 +0900

    week6

---
+++ CocoapodsLibrary/CollectionView/Cell/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  CocoapodsLibrary
+//
+//  Created by 김미주 on 24/05/2024.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ CocoapodsLibrary/CollectionView/CollectionViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CollectionViewController.swift
+//  CocoapodsLibrary
+//
+//  Created by 김미주 on 24/05/2024.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
