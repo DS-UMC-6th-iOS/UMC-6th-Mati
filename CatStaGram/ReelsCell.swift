63db826e2622d183e1b90dfad5924ba2def21c0b
miju <alwn8918@gmail.com>
2024-06-28 09:27:17 +0900

    섹션 10: 릴스 화면 구성 및 기능 구현

---
+++ CatStaGram/ReelsCell.swift
@@ -0,0 +1,8 @@
+//
+//  ReelsCell.swift
+//  CatStaGram
+//
+//  Created by 김미주 on 28/06/2024.
+//
+
+import Foundation
---
+++ CatStaGram/VideoPlayerView.swift
@@ -0,0 +1,8 @@
+//
+//  VideoPlayerView.swift
+//  CatStaGram
+//
+//  Created by 김미주 on 28/06/2024.
+//
+
+import Foundation
