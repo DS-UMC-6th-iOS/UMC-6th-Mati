36f36b5146e6bf2de8d060e0f63ed5935eb59c1c
김미주 <alwn8918@gmail.com>
2024-07-09 16:14:50 +0900

    화면 간 전환

---
+++ baemin_order/OrderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OrderTableViewCell.swift
+//  baemin_order
+//
+//  Created by 김미주 on 19/05/2024.
+//
+
+import UIKit
+
+class OrderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ baemin_order/OrderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrderViewController.swift
+//  baemin_order
+//
+//  Created by 김미주 on 20/05/2024.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
